1b813dd79b5abf40051445e3cb1345858d1e9c17
ZdravkoD <kfzzdravko@gmail.com>
2015-06-20 14:25:53 +0300

    Dobavih class v reshenieto

--- week1/5-Sorting/Count Sort.cpp
+++ week1/5-Sorting/Count Sort.cpp
@@ -6,22 +6,22 @@ using namespace std;
 
 #define LEN 20
 
-void FindMinMax(int &Min,int &Max,int *Arr)
-{
+class CountSort {
+  void FindMinMax(int &Min,int &Max,int *Arr)  {
     Min=INT_MAX;
     Max=INT_MIN;
 
-    for(int i=0;i<LEN;i++)
-    {
-        if(Arr[i]<Min)
-            Min=Arr[i];
-        if(Arr[i]>Max)
-            Max=Arr[i];
+    for(int i=0;i<LEN;i++)  {
+      if(Arr[i]<Min)
+          Min=Arr[i];
+      if(Arr[i]>Max)
+          Max=Arr[i];
+      }
     }
-}
 
-int* Sort(int *Arr)
-{
+public:
+
+  int* Sort(int *Arr) {
     int Min,Max;
     FindMinMax(Min,Max,Arr);
 
@@ -29,29 +29,30 @@ int* Sort(int *Arr)
     int *Counting=new int[Size];
 
     for(int i=0;i<Size;i++)
-        Counting[i]=0;
+      Counting[i]=0;
 
     int Add = (Min<0 ? abs(Min) : 0);
 
     for(int i=0;i<LEN;i++)
-        Counting[Arr[i]+Add]++;
+      Counting[Arr[i]+Add]++;
 
     int Index=0; // Arr Index
-    for(int i=0;i<Size;i++)
-    {
-        while(Counting[i]-->0)
-        {
-            Arr[Index++]=i-Add;
+    for(int i=0;i<Size;i++) {
+      while(Counting[i]-->0)  {
+        Arr[Index++]=i-Add;
         }
-    }
+      }
 
-    return Arr;
-}
+  return Arr;
+  }
+};
+
 
 int main()
 {
     int Arr[LEN]={1,15,-645,34,65,876,-3,-7,9,10,23,55,-567,7,97,-97,98,75,45,32};
-    Sort(Arr);
+    CountSort S;
+    S.Sort(Arr);
 
     for(int i=0;i<LEN;i++)
         cout << Arr[i] << " ";
--- week1/5-Sorting/Selection Sort.cpp
+++ week1/5-Sorting/Selection Sort.cpp
@@ -4,31 +4,32 @@ using namespace std;
 
 #define LEN 20
 
-int* Sort(int *Arr)
-{
-    for(int i=0;i<LEN;i++)
-    {
-        int Min=Arr[i];
-        int Index=i;
-        for(int j=i+1;j<LEN;j++)
-        {
-            if(Arr[j]<Min)
-            {
-                Min=Arr[j];
-                Index=j;
-            }
+class SelectionSort {
+
+public:
+  int* Sort(int *Arr)  {
+    for(int i=0;i<LEN;i++)  {
+      int Min=Arr[i];
+      int Index=i;
+      for(int j=i+1;j<LEN;j++)  {
+        if(Arr[j]<Min)  {
+          Min=Arr[j];
+          Index=j;
+          }
         }
-        int temp=Arr[i];
-        Arr[i]=Arr[Index];
-        Arr[Index]=temp;
-    }
+      int temp=Arr[i];
+      Arr[i]=Arr[Index];
+      Arr[Index]=temp;
+      }
     return Arr;
-}
+    }
+};
 
 int main()
 {
     int Arr[LEN]={1,15,-645,34,65,876,-3,-7,9,10,23,55,-567,7,97,-97,98,75,45,32};
-    Sort(Arr);
+    SelectionSort S;
+    S.Sort(Arr);
 
     for(int i=0;i<LEN;i++)
         cout << Arr[i] << " ";
