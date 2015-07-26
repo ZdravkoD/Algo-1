4f94e062a608f2c04d9735361023b9f7794b7d6b
Zdravko <kfzzdravko@gmail.com>
2015-07-26 08:45:59 +0300

    Update roots.cpp

--- week2/1-Roots/roots.cpp
+++ week2/1-Roots/roots.cpp
@@ -1,8 +1,39 @@
-class Roots {
-public:
+#include <stdio.h>
 
-  // Finds the square root of a number using binary search.
-  double squareRoot(int number) {
-    // ...
-  }
+using namespace std;
+
+const double Eps=0.000001;
+
+// function to calculate root
+class Root  {
+
+public:
+  double Square(const double &number)  {
+    if(number<0)
+      return -1;
+
+    double L=0, R=number;
+
+    while(L<=R) {
+      double Mid = L + (R - L)/2;
+      double Answ = Mid * Mid - number;
+      if(Answ < 0 && Answ < -Eps)
+        L = Mid;
+      else if(Answ > 0 && Answ > Eps)
+        R = Mid;
+      else
+        return Mid;
+      }
+    }
 };
+
+int main()
+{
+  Root R;
+
+  int n;
+  scanf("%d",&n);
+  printf("%.5f\n", R.Square(n));
+
+  return 0;
+}
