e79d95e18d6237eac07b05ea409ed5e5ff84d919
Zdravko <kfzzdravko@gmail.com>
2015-07-26 08:50:33 +0300

    Update k_lists.cpp

--- week2/6-K-Lists/k_lists.cpp
+++ week2/6-K-Lists/k_lists.cpp
@@ -1,6 +1,11 @@
-#include <iostream>
 #include <vector>
 #include <algorithm>
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <fstream>
+#include <ctime>
+#include <list>
 
 using namespace std;
 
@@ -8,38 +13,60 @@ class Node {
 public:
   int value;
   Node* next;
+  Node(int val = 0, Node* nxt = NULL):value(val),next(nxt){};
 };
 
-
 class KLists {
-
-private:
-  static inline bool Comp(const int &A, const int &B)  {
-    return A<B;
-    }
-
 public:
 
+static inline bool comp(const int& a, const int& b)
+{
+    return (a<=b);
+}
+
   // Merge K sorted lists.
-  Node merge(vector<Node> &lists)  {
-    Node Result;
-    vector<int> TooSlow;
-    int len = lists.size();
-    for(int i=0;i<len;i++)  {
-      while(lists[i].next)  {
-        TooSlow.push_back(lists[i].value);
-        lists[i] = *(lists[i].next);
-        }
-      TooSlow.push_back(lists[i].value);
-      }
-    sort(TooSlow.begin(),TooSlow.end());
+    Node merge(vector<Node>& lists) {
+    vector<int> too_slow;
+    for(int i=0; i<lists.size(); i++)
+    {
+       while(lists[i].next)
+       {
+           too_slow.push_back(lists[i].value);
+           lists[i] = *(lists[i].next);
+       }
+       too_slow.push_back(lists[i].value);
+    }
+    sort(too_slow.begin(), too_slow.end() );
+
+    int input_int;
+    input_int = too_slow[0];
+    Node result(input_int,NULL);
+    Node *Last = &result;
+    for(int i=1; i<too_slow.size(); i++) {
+      Node *NewNode = new Node(too_slow[i],NULL);
+      Last->next = NewNode;
+      Last = NewNode;
+    }
+    return result;
     }
 };
 
 int main()
 {
-  KLists L;
-  //L.merge();
+  cin.sync_with_stdio(false);
+  cin.tie(0);
+
+  vector<int> lists;
+  int k;
+  cin >> k;
+  while(cin >> k) {
+    if(k!=-1)
+      lists.push_back(k);
+  }
+  sort(lists.begin(),lists.end());
+  for(int i=0;i<lists.size();i++) {
+    cout << lists[i] << " ";
+  }
 
   return 0;
 }
